1fe358e9dc9789eb2b76b5fcef9eec2c4e4aeeff
Alex Kharunzha <a.kharunzha@gmail.com>
2019-03-30 21:10:13 +0200

    Simfony, auth, register, logout, user entity with table
    /login - ugly but working login form
    /register - register form. No password format validation yet
    /logout - path says by himself
    /profile - "Hi, user name" page
    / - default page
    
    do not forget to run migration.

---
+++ project/src/Controller/ProfileController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function index()
+    {
+        // usually you'll want to make sure the user is authenticated first
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // returns your User object, or null if the user is not authenticated
+        // use inline documentation to tell your editor your exact User class
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+            'user'=>$user
+        ]);
+    }
+}
