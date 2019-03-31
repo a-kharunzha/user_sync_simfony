8621d11113eb5b06a0dc03fe7f0a43a0f6988f83
a-kharunzha <a.kharunzha@gmail.com>
2019-03-31 20:31:35 +0300

    redirect after success update, success message to session

--- project/src/Controller/ProfileController.php
+++ project/src/Controller/ProfileController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\ProfileFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -14,11 +16,13 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile", name="profile")
      */
-    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function index(Request $request, SessionInterface $session)
     {
         // usually you'll want to make sure the user is authenticated first
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
+        //
+        $session->start();
+        $message = $session->remove('profile_message');
         // returns your User object, or null if the user is not authenticated
         // use inline documentation to tell your editor your exact User class
         /** @var User $user */
@@ -27,16 +31,15 @@ class ProfileController extends AbstractController
 
         $form = $this->createForm(ProfileFormType::class, $user);
         $form->handleRequest($request);
-
-        $message = '';
-
+        //
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-            $message = 'Profile data is updated';
+            $session->set('profile_message','Profile data is updated');
+            return new RedirectResponse('profile');
         }
-
+        // view
         return $this->render('profile/profile.html.twig', [
             'controller_name' => 'ProfileController',
             'user'=>$user,
